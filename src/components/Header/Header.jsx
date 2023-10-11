2dc47a84cab27b0521f1b45919ecc1e4b499f77e
Chris <chris@Chriss-MacBook-Air.local>
2023-10-11 22:20:01 +1100

    navbar dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,28 @@
 import React from "react";
 import Button from "../Button/Button";
-import imgOne from "../../assets/images/blackwoodBondi.jpg";
-import imgTwo from "../../assets/images/blacwoodCron.jpg";
 import headerStyle from "./Header.module.css";
 
 const Header = () => {
-  const customButtonStyles = {
-  
-    position: 'absolute',
-    bottom: '175px',
-    left:"400px",
+  const buttonStyle = {
+    color: "white",
+    zIndex: "3",
+
   };
-
-
   return (
     <header>
+      <div className={headerStyle.overlay}></div>
+
       <div className={headerStyle.containerOne}>
-        <img src={imgOne} alt="image Blackwood Bondi" />
-        <Button customStyles={customButtonStyles} button="Explore Cronulla" />
+
+        <div>
+          <Button button="Explore Cronulla" customStyles={buttonStyle} />
+        </div>
       </div>
       <div className={headerStyle.containerTwo}>
-        <img src={imgTwo} alt="image Blackwood Cronulla" />
-        <div className={headerStyle.positionsBtn}>
-        <Button /* customStyles={customButtonStylesBondi} */ button="Booking" />
-        <Button /* customStyles={customButtonStyles} */ button="Explore Bondi" />
+        <div>
+          <Button button="Booking" customStyles={buttonStyle} />
+          <Button button="Explore Bondi" customStyles={buttonStyle} />
         </div>
-
       </div>
     </header>
   );
