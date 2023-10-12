c497121ddc30e0be9a3b700ce39bd340260877b7
Chris <chris@Chriss-MacBook-Air.local>
2023-10-12 16:45:22 +1100

    whatson done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Nav from "../components/Nav/Nav"
 import Header from "../components/Header/Header"
 import About from "../components/About/About"
 import Menus from '../components/Menus/Menus'
+import WhatsOn from '../components/WhatsOn/WhatsOn'
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
     <Header />
     <About />
     <Menus/>
+    <WhatsOn />
     </>
   )
 }
