f638d4971ebe54fbc23a18f6a722dc35a495e2a6
Chris <chris@Chriss-MacBook-Air.local>
2023-10-12 15:49:01 +1100

    transition menu done

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styleMenu from "./Menus.Module.css"
 import MenuBondi from '../MenuBondi/MenuBondi'
 import MenuCronulla from '../MenuCronulla/MenuCronulla'
 import Button from '../Button/Button'
 
 const Menus = () => {
-  
+  const [selectedMenu, setSelectedMenu] = useState('bondi');
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+
+ 
+
+  const handleOptionClick = (menu) => {
+    if (selectedMenu !== menu) {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setSelectedMenu(menu);
+        setIsTransitioning(false);
+      }, 500);
+    }
+  }
   return (
     <div className={styleMenu.menuContainer}>
     <div className={styleMenu.menusCnt}>
         <h2>MENUS</h2>
         <div className={styleMenu.menusP}>
-            <p>Bondi</p>
-            <p>Cronulla</p>
+
+        <p onClick={() => handleOptionClick('bondi')} className={selectedMenu === 'bondi' ? styleMenu.optionActive : ""}>Bondi</p>
+        <p onClick={() => handleOptionClick('cronulla')} className={selectedMenu === 'cronulla' ? styleMenu.optionActive : ""}>Cronulla</p>
         </div>
-        <div className={styleMenu.menusBox} >
-         <MenuCronulla/> 
-        </div>
+        <div
+  className={
+    styleMenu.menusBox +
+    (isTransitioning ? " " + styleMenu["transition-out"] : "")
+  }
+>
+  {selectedMenu === "bondi" ? <MenuBondi /> : <MenuCronulla />}
+</div>
+
+
 
          <Button button="book now"/>
 
